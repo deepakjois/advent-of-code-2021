6597cc5b6102dc5e5008c66cf463a147f7e60f56
Deepak Jois <deepak.jois@gmail.com>
2021-12-09 20:21:08 -0800

    minor

--- day9/day9.py
+++ day9/day9.py
@@ -5,23 +5,22 @@ from functools import reduce
 
 grid = [[int(c) for c in line.strip()] for line in sys.stdin]
 
-def valid_neighbors(loc):
+def neighbors(x,y):
     dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    x, y = loc
     for (dx, dy) in dirs:
          if x + dx >= 0 and (x + dx) < len(grid) and (y + dy) >= 0 and (y + dy) < len(grid[0]):
              yield (x+dx, y+dy)
 
-def bfs(loc):
+def basin_size(x,y):
     q = queue.Queue()
     v = set()
-    q.put(loc)
-    v.add(loc)
+    q.put((x,y))
+    v.add((x,y))
     size = 1
     while not q.empty():
         x, y = q.get()
         val = grid[x][y]
-        for (nx, ny) in valid_neighbors((x,y)):
+        for nx, ny in neighbors(x,y):
             if (nx, ny) in v:
                 continue
             adj = grid[nx][ny]
@@ -36,15 +35,14 @@ basins = []
 for (x,y) in product(range(len(grid)), range(len(grid[0]))):
     val = grid[x][y]
     min = True
-    for (nx, ny) in valid_neighbors((x,y)):
+    for (nx, ny) in neighbors(x,y):
         adj = grid[nx][ny]
         if val >= adj:
             min = False
             break
     if min:
         risk_level = risk_level + val + 1
-        basin_size = bfs((x,y))
-        basins.append(basin_size)
+        basins.append(basin_size(x,y))
 
 print(risk_level)
 basins.sort(reverse=True)
